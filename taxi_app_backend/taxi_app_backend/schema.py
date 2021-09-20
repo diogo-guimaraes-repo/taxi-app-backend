1b1f84dc43d2eeba5cd6eaa00713d11729ddb0ec
Diogo <diogo.m.guimaraes.92@gmail.com>
2021-09-20 15:05:47 +0100

    Password reset and user verification implementation

--- taxi_app_backend/taxi_app_backend/schema.py
+++ taxi_app_backend/taxi_app_backend/schema.py
@@ -8,6 +8,8 @@ class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
 
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
